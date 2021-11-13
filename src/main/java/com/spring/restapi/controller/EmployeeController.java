d6e0ac5fa20b6b91a8fc25a83621c8dbe9622c73
GiaBaone <102190252@sv1.dut.udn.vn>
2021-11-13 12:33:28 +0700

    test api using postman 13.11.21

--- src/main/java/com/spring/restapi/controller/EmployeeController.java
+++ src/main/java/com/spring/restapi/controller/EmployeeController.java
@@ -1,11 +1,9 @@
 package com.spring.restapi.controller;
 
+import com.spring.restapi.model.Employee;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.HttpMethod.*;
 
@@ -13,11 +11,30 @@ import static org.springframework.http.HttpMethod.*;
 public class EmployeeController {
 
     //localhost:8080/employees
-    @RequestMapping(value= "/employees", method = RequestMethod.GET)
-    @ResponseBody
-    public String getEmployee(){
+    @GetMapping("/employees")
+    public String getEmployees(){
+
         return "displying the list of employees";
     }
 
+    @GetMapping("/employees/{id}")
+    public String getEmployee(@PathVariable Long id){
+        return "Fetching the employee details for the id "+ id;
+    }
+    @PostMapping("/employees")
+    public String saveEmployee(@RequestBody Employee employee) {
+        return "saving the employee details to the database" + employee;
+    }
+
+    @PutMapping("/employees/{id}")
+    public Employee updateEmployee(@PathVariable Long id, @RequestBody Employee employee){
+        System.out.println("update the employee data for the id" + id);
+        return employee;
+    }
+
+    @DeleteMapping("/employees")
+    public String deleteEmployee(@RequestParam("id") Long id){
+        return "deleting the employee detail has id: " +id;
+    }
 
 }
---
+++ src/main/java/com/spring/restapi/model/Employee.java
@@ -0,0 +1,18 @@
+package com.spring.restapi.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Employee {
+    private String name;
+    private Long age;
+    private String location;
+    private String email;
+    private String department;
+
+
+}
